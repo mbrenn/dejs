44cbd899bcf1519f254c02012ce94374df79176e
Martin Brenn <brenn@depon.net>
2013-01-24 13:15:19 +0100

    dejs.table is working in a very simple way

--- src/js/dejs.table.js
+++ src/js/dejs.table.js
@@ -1,25 +1,78 @@
 define([],
     function () {
         var tableClass = function (domElement) {
+            if (domElement === undefined) {
+                throw ("First parameter 'domElement' is not defined.");
+            }
             this.__domElement = domElement;
+            this.__currentTable = undefined;
             this.__currentRow = undefined;
             this.__isHeaderRow = false;
+            
+            this.__init();
         };
 
         tableClass.prototype =
             {
-                addRow: function () {
-                    // TODO: Start new row with td
+                __init: function () {
+                    this.__currentTable = $("<table></table>");
+                    this.__domElement.append(this.__currentTable);
                 },
 
-                addHeaderRow: function()
-                {
-                    // TODO: Start new row with th
+                addRow: function () {
+                    this.__isHeaderRow = false;
+
+                    this.__currentRow = $("<tr></tr>");
+                    this.__currentTable.append(this.__currentRow);
+
+                    return this.__currentRow;
+                },
+
+                addHeaderRow: function () {
+                    this.__isHeaderRow = true;
+
+                    this.__currentRow = $("<tr></tr>");
+                    this.__currentTable.append(this.__currentRow);
+
+                    return this.__currentRow;
                 },
 
                 addColumn: function (content, options) {
-                    // TODO: Add new Column
+                    var currentColumn = this.__createColumn(options);
+                    currentColumn.text(content);
+                },
+
+                addColumnHtml: function (content, options) {
+                    var currentColumn = this.__createColumn(options);
+                    currentColumn.html(content);
+
+                    return currentColumn;
+                },
+
+                __createColumn: function (options) {
+                    if (this.__currentRow === undefined) {
+                        throw ("Row has not been started");
+                    }
+
+                    if (options == undefined) {
+                        options = {};
+                    }
+
+                    var currentColumn;
+
+                    if (this.__isHeaderRow || options.asHeader === true) {
+                        currentColumn = $("<th></th>");
+                    }
+                    else {
+                        currentColumn = $("<td></td>");
+                    }
+
+                    this.__currentRow.append(currentColumn);
+
+                    return currentColumn;
                 }
             };
+
+        return tableClass;
     }
     );
