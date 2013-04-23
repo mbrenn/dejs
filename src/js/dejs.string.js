c07854a8fdc91dd86f2d83c6974f7e510e091eee
Martin Brenn <brenn@depon.net>
2013-04-23 22:58:04 +0200

    Start of dejs.string

---
+++ src/js/dejs.string.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+define([],
+	function () {
+		var methods = {
+			encodeHtml: function (text) {
+				return $("<div></div>").text(text).html();
+			}
+		};
+
+		return methods;
+	});
