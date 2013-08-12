b71de04864dcf23e4c04b02d6a99f2f2c90869d3
Martin Brenn <brenn@depon.net>
2013-08-12 21:45:59 +0200

    Transfered library to typescript, not compatible to old version

---
+++ src/js/dejs.string.ts
@@ -0,0 +1,14 @@
+/// <reference path="lib/jquery.d.ts" />
+
+export class String
+{
+	encodeHtml(text : string)
+	{
+		return $("<div></div>").text(text).html();
+	}
+	
+	nl2br(text : string)
+	{
+		return text.replace(/\n/g, "<br />");
+	}
+}
---
+++ src/js/dejs.table.ts
@@ -0,0 +1,79 @@
+/// <reference path="lib/jquery.d.ts" />
+
+export class CellOptions
+{
+	asHeader : bool;
+}
+
+export class Table
+{
+	__domElement : JQuery;
+	__currentTable : JQuery;
+	__currentRow : JQuery;
+	__isHeaderRow : bool;
+	
+	constructor(domElement : JQuery)
+	{
+		this.__domElement = domElement;
+		this.__currentTable = $("<table></table>");
+		this.__domElement.append(this.__currentTable);
+	}
+	
+	addRow()
+	{
+		this.__isHeaderRow = false;
+
+		this.__currentRow = $("<tr></tr>");
+		this.__currentTable.append(this.__currentRow);
+
+		return this.__currentRow;
+	}
+	
+	addHeaderRow()
+	{
+		this.__isHeaderRow = true;
+
+		this.__currentRow = $("<tr></tr>");
+		this.__currentTable.append(this.__currentRow);
+
+		return this.__currentRow;
+	}
+
+	addColumn(content : string, options) {
+		var currentColumn = this.__createColumn(options);
+		currentColumn.text(content);
+
+		return currentColumn;
+	}
+
+	addColumnHtml(content : string, options) {
+		var currentColumn = this.__createColumn(options);
+		currentColumn.html(content);
+
+		return currentColumn;
+	}
+	
+	__createColumn(options) 
+	{
+		if (this.__currentRow === undefined) {
+			throw ("Row has not been started");
+		}
+
+		if (options == undefined) {
+			options = {};
+		}
+
+		var currentColumn;
+
+		if (this.__isHeaderRow || options.asHeader === true) {
+			currentColumn = $("<th></th>");
+		}
+		else {
+			currentColumn = $("<td></td>");
+		}
+
+		this.__currentRow.append(currentColumn);
+
+		return currentColumn;
+	}
+}
